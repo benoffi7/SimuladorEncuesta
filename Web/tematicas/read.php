1be536bfb65f8ff9600cd97f47de19da1fc503db
Leticia <leticia_berterreche@edsa.com.ar>
2017-11-25 09:54:16 -0300

    crud tematicas

---
+++ Web/tematicas/read.php
@@ -0,0 +1,81 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once 'config.php';
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM tematicas WHERE id_tematicas = ?";
+    
+    if($stmt = $mysqli->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bind_param("i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            $result = $stmt->get_result();
+            
+            if($result->num_rows == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $descripcion = $row["descripcion"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Ocurrió un error. Intente nuevamente más tarde.";
+        }
+    }
+     
+    // Close statement
+    $stmt->close();
+    
+    // Close connection
+    $mysqli->close();
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Detalle</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Detalle</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Descripción</label>
+                        <p class="form-control-static"><?php echo $row["descripcion"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Volver</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
